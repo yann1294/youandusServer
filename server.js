6c4bb8ad10e1230a6a26fddbe814e8d6404c6a2a
Salazar1204 <aguidissou.abrahamyann@adgroupe.io>
2023-03-10 01:06:36 +0530

    updated request storage

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ import { readdirSync } from "fs";
 import mongoose from "mongoose";
 import csrf from "csurf";
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import crypto from 'crypto';
 const morgan = require("morgan");
 require("dotenv").config();
-
-const csrfProtection = csrf({ cookie: true });
-const crypto = require('crypto')
-const session = require('express-session')
 
 // create express app
 const app = express();
@@ -25,24 +23,37 @@ mongoose
   .then(() => console.log("**DB CONNECTED**"))
   .catch((err) => console.log("DB CONNECTION ERR => ", err));
 
-
 // apply middlewares
 app.use(cors({
   credentials: true,
-  origin: 'https://youandusclient.vercel.app'
+  origin: 'https://trial-umber.vercel.app'
 }));
 app.use(express.json({limit: '5mb'}));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+// add express-session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    }
+  })
+);
+
 // route
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 // csrf
+const csrfProtection = csrf({ cookie: true });
 app.use(csrfProtection);
 
 app.get("/api/csrf-token", (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://youandusclient.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://trial-umber.vercel.app');
   res.header('Access-Control-Expose-Headers', 'X-CSRF-Token');
   res.json({ csrfToken: req.csrfToken() });
 });
